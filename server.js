138bbab90114d483db2c757d63cca3e96b402bda
FeuerwehrAUSTRIAX <feuerwehr.wienerneustadt.ax@gmail.com>
2026-03-12 11:04:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/api/einsaetze", async (req, res) => {
   try {
-    const r = await fetch(
+    const response = await fetch(
       "https://einsatzuebersicht.lfv.steiermark.at/einsatzkarte/data/public_current.json"
     );
 
-    const data = await r.text();
+    const data = await response.text();
 
     res.setHeader("Content-Type", "application/json");
     res.send(data);
+
   } catch (err) {
     res.status(500).json({
       error: "Proxy error",
@@ -22,9 +23,9 @@ app.get("/api/einsaetze", async (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Proxy läuft ✅");
+  res.send("Proxy läuft!");
 });
 
 app.listen(PORT, () => {
-  console.log("Server läuft auf Port", PORT);
+  console.log("Server läuft auf Port " + PORT);
 });
